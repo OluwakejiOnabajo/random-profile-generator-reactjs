85e46964af505fe8d12c12d4b92c52d7844c4f16
Oluwakeji <onabajooluwakeji.daniel@gmail.com>
2022-08-16 14:24:41 +0100

    more push

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,6 @@ import { Link} from "react-router-dom";
 import axios from "axios";
 
 const Profile = () => {
-  // const [firstname, setFirstname] = useState('');
   const [personData, setpersonData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [activeProfile, setActiveProfile] = useState(false);
@@ -14,10 +13,10 @@ const Profile = () => {
     setLoading(true);
     axios.get('https://randomuser.me/api/')
     .then( (response) => {
-      console.log(response.data.results);
+      // console.log(response.data.results);
       setpersonData(response.data.results);
     }).catch((error) => {
-      console.log(error);
+      // console.log(error);
       setLoading(true);
     }).finally(() => {
       setLoading(false);
@@ -29,14 +28,14 @@ const Profile = () => {
     
     <>
     <div className='profile-card'>
-      { loading ? <i>Loading...</i> : <div>
+      { loading ? <p><i>Loading...</i></p> : <div>
         {
           personData.map((person, index) => {
             return (
-              <div key={person.cell} >
+              <div className='details' key={person.cell} >
               <img src={person.picture.large} className='user-img' alt='user-img' />
               <p>My name is</p>
-              <h2>{person.name.first}</h2>
+              <h2>{person.name.first + ' '+ person.name.last}</h2>
               </div>
             )
           })
@@ -44,7 +43,7 @@ const Profile = () => {
       </div>
 
       }
-      <button onClick={profileHandler} type='button' className='user-btn' >{activeProfile ? "Get Another Profile" : "Get Profile"}</button>
+      <p className='button-div'><button onClick={profileHandler} type='button' className='user-btn' >{activeProfile ? "Get Another Profile" : "Get Profile"}</button></p>
     </div>
     <p className='comment'>Made with <span>❣</span> by <Link to="/" onClick={() => { window.location.href= 'https://github.com/OnabajoOluwakeji'; }} >Oluwakeji Onabajo</Link></p>
     </>
